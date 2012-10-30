7f4e57fcbea340c228b204aacf27ff97349aa7ba
progman <gnuplanet@gmail.com>
2012-10-30 16:29:05 +0400

    set default update repository, not clone

--- git_backup_example.sh
+++ git_backup_example.sh
@@ -6,6 +6,7 @@ export GIT_BACKUP_DIR='/tmp/git_backup'; # dir for backups
 #export GIT_BACKUP_FLAG_USE_BZIP2=1;      # use bzip2, default auto
 #export GIT_BACKUP_FLAG_USE_XZ=1;         # use xz,    default auto
 #export GIT_BACKUP_MAX_ITEM_COUNT=0;      # max backups count. default 0 - disable
+#export GIT_BACKUP_FLAG_SMALL_AND_SLOW=0; # delete temporary cache. default 0 - disable
 
 
 #export GIT_BACKUP_REPO_LIST='REPO1, REPO2 BRANCH1 BRANCH2, REPO3, REPO4 BRANCH1';
@@ -16,7 +17,9 @@ export GIT_BACKUP_DIR='/tmp/git_backup'; # dir for backups
 # REPO4 - get ONLY BRANCH1
 export GIT_BACKUP_REPO_LIST="\
 git://github.com/progman/gitbash.git, \
-git://github.com/progman/git_backup.git master \
+git://github.com/progman/git_backup.git master, \
+/tmp/xxx, \
+/tmp/yyy \
 ";
 
 ./git_backup.sh
